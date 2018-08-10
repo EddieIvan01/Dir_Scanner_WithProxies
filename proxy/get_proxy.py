f99251b570777b688a154d6fd743335c53f48a0c
Eddie Ivan <33752542+EddieIvan01@users.noreply.github.com>
2018-08-10 10:16:39 +0800

    Create get_proxy.py

---
+++ proxy/get_proxy.py
@@ -0,0 +1,36 @@
+import requests
+import sys
+import random
+
+'''
+Decorator for dir_scanner
+using ProxyPool from
+https://github.com/eddieivan01/ProxyPool
+author: EddieIvan
+'''
+
+local = "http://127.0.0.1:2333"
+proxy_json = requests.get(local + "/proxy?act=get").json()
+global proxy_list
+proxy_list = []
+if proxy_json["code"] == 200:
+    for i in proxy_json["proxies"]:
+        proxy_list.append(
+            {
+                "http":"http://" + i["ip"] + ":" + i["port"],
+                "https":"https://" + i["ip"] + ":" + i["port"]
+            }
+        )
+else:
+    print("[*]request for proxy error")
+
+def RequestWithProxy(f):
+    try:
+        res = requests.get(local, timeout = 3)
+    except:
+        print("[*]Local server are not listening")
+        sys.exit(-1)
+    def foo(arg):
+        proxy = random.choice(proxy_list)
+        f(arg, proxy)
+    return foo
